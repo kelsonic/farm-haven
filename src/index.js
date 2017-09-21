408227563f5412a3bee5969c3a5fd6e367be4096
kelsonic <kgadams93@gmail.com>
2017-09-20 22:16:43 -0400

    Add foundation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
+// Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers} from 'redux';
+
+// Import create-react-app service worker (houses webpack, babel, etc. config)
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Components
+import App from './components/App';
+
+// Inject global styles
+import './globalStyles.css';
+
+// Import reducers and create shape of store
+import gameReducer from './containers/Game/reducer';
+const rootReducer = combineReducers({
+  game: gameReducer,
+});
+
+// Enable Redux DevTools if available
+const enableReduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+// Create store
+const store = createStore(rootReducer, enableReduxDevTools);
+
+ReactDOM.render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('root'));
 registerServiceWorker();
